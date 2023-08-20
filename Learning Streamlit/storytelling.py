1aa78a1dad1dc5c009271e90c5679f577de35c3d
Maisa Fraiz <maisafraiz@hotmail.com>
2023-08-20 14:05:31 -0300

    Update storytelling.py

--- Learning Streamlit/storytelling.py
+++ Learning Streamlit/storytelling.py
@@ -54,9 +54,65 @@ st.write("*Note*: I will not be putting the symbols $ or % like the example show
 
 st.write("The author then tweaks the table a little more, adding coloring to the cells; since my project does not focus on tables, I will not do this exercise.")
 
-teste = alt.Chart(exercise21).mark_bar().encode(
-    x='Tier',
-    y='% Accounts'
+exercise21.drop(exercise21.index[6], inplace=True)
+
+chart = alt.Chart(exercise21).mark_bar().encode(
+    y = alt.Y('value:Q', axis=alt.Axis(title='%')),
+    x=alt.X('variable:N', axis=alt.Axis(title='')),
+    color=alt.Color('variable:N', legend=alt.Legend(title='Metric')),
+    column=alt.Column('Tier:O', header=alt.Header(labelOrient='bottom'),
+                      sort = ['A+',  'A', 'B', 'C', 'D', 'All other', 'Total'])
+).transform_fold(
+    fold = ['% Accounts', '% Revenue'],
+    as_=['variable', 'value']
 )
 
-st.write(teste)
+st.altair_chart(chart, use_container_width=False)
+
+# Your dataframe and chart setup code here...
+
+# Create a line chart to connect the bars
+line_data = []
+
+for i in range(len(exercise21)):
+    tier = exercise21.loc[i, 'Tier']
+    accounts = exercise21.loc[i, '% Accounts']
+    revenue = exercise21.loc[i, '% Revenue']
+    
+    line_data.append({'Tier': tier, 'variable': '% Accounts', 'value': accounts, 'next_variable': '% Revenue', 'next_value': revenue})
+    line_data.append({'Tier': tier, 'variable': '% Revenue', 'value': revenue, 'next_variable': '% Accounts', 'next_value': accounts})
+
+line_df = pd.DataFrame(line_data)
+
+st.write(line_df)
+# Create the line chart
+line_chart = alt.Chart(line_df).mark_line().encode(
+    x=alt.X('Tier:N', axis=alt.Axis(title='Tier'), sort=None),
+    y=alt.Y('value:Q', axis=alt.Axis(title='Percentage')),
+    color=alt.Color('variable:N', legend=alt.Legend(title='Metric')),
+    detail='variable:N'
+)
+# Create the bar chart
+bar_chart = alt.Chart(exercise21).mark_bar().encode(
+    alt.X('Tier', sort=None),
+    y=alt.Y('% Accounts', axis=alt.Axis(title='%')),
+    color=alt.Color('variable:N', legend=alt.Legend(title='Metric')),
+    xOffset=alt.Column('variable:N', title=None)
+).transform_fold(
+    ['% Accounts', '% Revenue'],
+    as_=['variable', 'value']
+)
+
+# Combine the bar chart and line chart
+chart4 = alt.layer(
+    bar_chart,
+    line_chart
+).properties(
+    width=400  # Adjust the width of the chart as needed
+)
+
+st.altair_chart(chart3)
+
+
+
+st.altair_chart(chart4)
