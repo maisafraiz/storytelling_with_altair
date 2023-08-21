076f617ddf927077d51adb4683197ea7b4ac0e38
Maisa Fraiz <maisafraiz@hotmail.com>
2023-08-20 23:06:48 -0300

    Update storytelling.py

--- Learning Streamlit/storytelling.py
+++ Learning Streamlit/storytelling.py
@@ -11,7 +11,7 @@ import altair as alt
 st.title('Storytelling with Data!')
 
 
-st.write('This streamlit app is meant to learn how to use streamlit and practice the examples in the book *Storytelling with Data - Lets Practice!*')
+st.write('This streamlit app is meant to learn how to use streamlit/altair and practice the examples in the book *Storytelling with Data - Lets Practice!*')
 
 st.header('Exercise 2.1 - Improve this table')
 
@@ -44,11 +44,12 @@ exercise21.loc[len(exercise21)] = ["All other", other_account_num, other_account
 
 exercise21.loc[len(exercise21)] = ["Total", exercise21['# of Accounts'].sum(), exercise21['% Accounts'].sum(), exercise21['Revenue ($M)'].sum(), exercise21['% Revenue'].sum()]
 
+rounded_ver = exercise21.copy()
 
-exercise21['% Accounts'] = exercise21['% Accounts'].apply(lambda x: round(x))
-exercise21['Revenue ($M)'] = exercise21['Revenue ($M)'].apply(lambda x: round(x, 1))
+rounded_ver['% Accounts'] = exercise21['% Accounts'].apply(lambda x: round(x))
+rounded_ver['Revenue ($M)'] = exercise21['Revenue ($M)'].apply(lambda x: round(x, 1))
 
-st.dataframe(exercise21)
+st.dataframe(rounded_ver)
 
 st.write("*Note*: I will not be putting the symbols $ or % like the example shown in the book as it would turn the numbers into strings.")
 
@@ -65,54 +66,13 @@ chart = alt.Chart(exercise21).mark_bar().encode(
 ).transform_fold(
     fold = ['% Accounts', '% Revenue'],
     as_=['variable', 'value']
+).properties(title={
+      "text": ["New client tier share"], 
+      "subtitle": ["% OF TOTAL ACCOUNTS vs REVENUE"],
+      "subtitleColor": "blue"
+    }
 )
 
 st.altair_chart(chart, use_container_width=False)
 
-# Your dataframe and chart setup code here...
-
-# Create a line chart to connect the bars
-line_data = []
-
-for i in range(len(exercise21)):
-    tier = exercise21.loc[i, 'Tier']
-    accounts = exercise21.loc[i, '% Accounts']
-    revenue = exercise21.loc[i, '% Revenue']
-    
-    line_data.append({'Tier': tier, 'variable': '% Accounts', 'value': accounts, 'next_variable': '% Revenue', 'next_value': revenue})
-    line_data.append({'Tier': tier, 'variable': '% Revenue', 'value': revenue, 'next_variable': '% Accounts', 'next_value': accounts})
-
-line_df = pd.DataFrame(line_data)
-
-st.write(line_df)
-# Create the line chart
-line_chart = alt.Chart(line_df).mark_line().encode(
-    x=alt.X('Tier:N', axis=alt.Axis(title='Tier'), sort=None),
-    y=alt.Y('value:Q', axis=alt.Axis(title='Percentage')),
-    color=alt.Color('variable:N', legend=alt.Legend(title='Metric')),
-    detail='variable:N'
-)
-# Create the bar chart
-bar_chart = alt.Chart(exercise21).mark_bar().encode(
-    alt.X('Tier', sort=None),
-    y=alt.Y('% Accounts', axis=alt.Axis(title='%')),
-    color=alt.Color('variable:N', legend=alt.Legend(title='Metric')),
-    xOffset=alt.Column('variable:N', title=None)
-).transform_fold(
-    ['% Accounts', '% Revenue'],
-    as_=['variable', 'value']
-)
-
-# Combine the bar chart and line chart
-chart4 = alt.layer(
-    bar_chart,
-    line_chart
-).properties(
-    width=400  # Adjust the width of the chart as needed
-)
-
-st.altair_chart(chart3)
-
-
-
-st.altair_chart(chart4)
+st.caption("Note to self: I've been finding really difficult to translate some examples of the books, as I feel some things are easy in Excel but I cannot find similar documentaion for it in Altair (like adding the lines connecting the bars in the graph above).")
